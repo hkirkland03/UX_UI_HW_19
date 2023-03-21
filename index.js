c50f8ca8a5652d92e21b726b8f24ddd861fd4821
Hanna Kirkland <96560397+hkirkland03@users.noreply.github.com>
2023-03-20 21:23:16 -0700

    Add files via upload
    homework

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    var button = document.getElementById("myButton","myButton1");
+    if (button.style.backgroundColor === "#E8E6E6") {
+      button.style.backgroundColor = "#577969";
+    } else {
+      button.style.backgroundColor = "#E8E6E6";
+    }
+  }
